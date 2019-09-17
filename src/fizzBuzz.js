2a62b17632d033e5a63e7912df402fdb0e99670a
Oliver Antcliff <antcliffoc@gmail.com>
2019-09-17 14:35:47 +0100

    complete

---
+++ src/fizzBuzz.js
@@ -0,0 +1,40 @@
+var FizzBuzz = function FizzBuzz() {
+}
+
+FizzBuzz.prototype.isDivisibleBy = function(divisor, number) {
+    return (number % divisor === 0)
+  };
+
+FizzBuzz.prototype.play = function play(number) {
+  if (this.isDivisibleBy(15, number)) {
+    return 'FizzBuzz';
+  } else if (this.isDivisibleBy(5, number)) {
+    return 'Buzz';
+  } else if (this.isDivisibleBy(3, number)) {
+    return 'Fizz';
+  } else {
+    return number;
+  }
+};
+
+var fizzBuzz = new FizzBuzz();
+
+for (var i = 1; i <= 100; i++) {
+  console.log(fizzBuzz.play(i));
+}
+
+// FizzBuzz.prototype = {
+//   isFizz: function isFizz(number) {
+//       if (number % 3 === 0) {
+//         return true
+//       } else {
+//         return false
+//       };
+//   isBuzz: function isBuzz(number) {
+//     if (number % 5 === 0) {
+//       return true
+//     } else {
+//       return false
+//     };
+//   isFizzBuzz:
+// }
